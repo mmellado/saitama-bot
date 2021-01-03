ccfdba7a3ee060ba9f1dbebc89e03ea9f54455a7
Marcos Mellado <marcos@mellados.com>
2021-01-03 18:56:13 +0100

    Add promise checks

--- src/commands/code.ts
+++ src/commands/code.ts
@@ -13,39 +13,43 @@ import isAdminUser from '../utils/isAdminUser';
 dotenv.config();
 
 const code: CommandPromise = async (client, message, args) => {
-  if (!isAdminUser(message.member?.roles.cache as Collection<string, Role>)) {
-    return;
+  try {
+    if (!isAdminUser(message.member?.roles.cache as Collection<string, Role>)) {
+      return;
+    }
+    if (!args?.length) {
+      message.channel.send('A code is needed to use this command');
+      return;
+    }
+
+    const newCode = args[0];
+
+    const filter: CollectorFilter = (response: Message) =>
+      response.author.id === message.author.id;
+
+    message.channel.send(`What's this code's reward?`);
+    const reply = await message.channel.awaitMessages(filter, {
+      max: 1,
+      time: 30000,
+      errors: ['time'],
+    });
+
+    const replyChannel =
+      (client.channels.cache.find(
+        (channel: TextChannel) => channel.name === process.env.CODES_CHANNEL
+      ) as TextChannel) || message.channel;
+
+    const embed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('New Code!')
+      .setDescription(newCode)
+      .addFields({ name: 'Rewards', value: reply.first()?.content })
+      .setTimestamp();
+
+    replyChannel.send(embed);
+  } catch (err) {
+    console.error(err);
   }
-  if (!args?.length) {
-    message.channel.send('A code is needed to use this command');
-    return;
-  }
-
-  const newCode = args[0];
-
-  const filter: CollectorFilter = (response: Message) =>
-    response.author.id === message.author.id;
-
-  message.channel.send(`What's this code's reward?`);
-  const reply = await message.channel.awaitMessages(filter, {
-    max: 1,
-    time: 30000,
-    errors: ['time'],
-  });
-
-  const replyChannel =
-    (client.channels.cache.find(
-      (channel: TextChannel) => channel.name === process.env.CODES_CHANNEL
-    ) as TextChannel) || message.channel;
-
-  const embed = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle('New Code!')
-    .setDescription(newCode)
-    .addFields({ name: 'Rewards', value: reply.first()?.content })
-    .setTimestamp();
-
-  replyChannel.send(embed);
 };
 
 export default code;
--- src/commands/nickname.ts
+++ src/commands/nickname.ts
@@ -1,13 +1,17 @@
 import { CommandPromise } from './types';
 
 const nickname: CommandPromise = async (_, message, args) => {
-  const newNickname = (args && args.join(' ')) || '';
-  await message.member?.setNickname(newNickname);
+  try {
+    const newNickname = (args && args.join(' ')) || '';
+    await message.member?.setNickname(newNickname);
 
-  if (!newNickname) {
-    message.channel.send('Your nickname has been reset');
-  } else {
-    message.channel.send(`Your nickname has been updated to ${newNickname}`);
+    if (!newNickname) {
+      message.channel.send('Your nickname has been reset');
+    } else {
+      message.channel.send(`Your nickname has been updated to ${newNickname}`);
+    }
+  } catch (err) {
+    console.error(err);
   }
 };
 
