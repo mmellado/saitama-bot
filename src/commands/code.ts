d4645e88744e66db2b0604e4566fd7bf934f3f00
Marcos Mellado <marcos@mellados.com>
2021-01-05 15:36:00 +0100

    Update code command to use post channel from settings

--- src/commands/code.ts
+++ src/commands/code.ts
@@ -12,13 +12,19 @@ import isAdminUser from '../utils/isAdminUser';
 
 dotenv.config();
 
-const code: CommandPromise = async (_server, message, args) => {
+const code: CommandPromise = async (server, message, args) => {
   try {
     if (!isAdminUser(message.member?.roles.cache as Collection<string, Role>)) {
       return;
     }
+
+    const conversationEmbed = new Discord.MessageEmbed()
+      .setColor('#ff0000')
+      .setTitle('Create new code - Error!')
+      .setDescription('A code is needed to use this command');
+
     if (!args?.length) {
-      message.channel.send('A code is needed to use this command');
+      message.channel.send(conversationEmbed);
       return;
     }
 
@@ -27,19 +33,28 @@ const code: CommandPromise = async (_server, message, args) => {
     const filter: CollectorFilter = (response: Message) =>
       response.author.id === message.author.id;
 
-    await message.channel.send(`What's this code's reward?`);
+    conversationEmbed
+      .setColor('#0099ff')
+      .setTitle('Create new code')
+      .setDescription("Please specify the code's reward");
+
+    const prompt = await message.channel.send(conversationEmbed);
     const reply = await message.channel.awaitMessages(filter, {
       max: 1,
       time: 30000,
       errors: ['time'],
     });
+    await reply.first()?.delete();
 
     const reward = reply.first()?.content || 'Error fetchign reward';
+    const replyChannel = message.client.channels.cache.get(
+      server.codeChannel
+    ) as TextChannel;
 
-    const replyChannel =
-      (message.client.channels.cache.find(
-        (channel: TextChannel) => channel.name === process.env.CODES_CHANNEL
-      ) as TextChannel) || message.channel;
+    conversationEmbed
+      .setColor('#00ff00')
+      .setTitle('Create new code - Success!')
+      .setDescription(`Code posted to <#${server.codeChannel}>`);
 
     const embed = new Discord.MessageEmbed()
       .setColor('#0099ff')
@@ -49,6 +64,7 @@ const code: CommandPromise = async (_server, message, args) => {
       .setTimestamp();
 
     await replyChannel.send(embed);
+    await prompt.edit(conversationEmbed);
   } catch (err) {
     console.error('Error on codes.ts', err);
   }
